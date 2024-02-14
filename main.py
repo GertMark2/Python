029b23638288656d8d4a7d10d757a63c99d09afe
Герт Марк Дмитриевич <GertM@lcoast.itstep>
2024-02-14 16:02:03 +0600

    print

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ class Crug:
 
     def __truediv__(self,other):
         return Crug(self.radius / other.radius)
+
     def __sub__(self,other):
         return Crug(self.radius - other.radius)
+
     def __str__(self):
         return str(self.radius)
+
+
 x = Crug(10)
 y = Crug(2)
 print(x+y)
@@ -21,3 +25,8 @@ print(x*y)
 print(x+y)
 print(x+y)
 # sub mul truediv add
+print("hellow")
+print("hellow")
+print("hellow")
+print("hellow")
+print("hellow")
